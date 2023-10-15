77a37341efd49c298f9c832230abc5668710c17f
IHaveNoIdeaWhatImDoing1234 <148015404+IHaveNoIdeaWhatImDoing1234@users.noreply.github.com>
2023-10-15 15:52:09 -0700

    Module 1 - Add, Subtract, Multiply, Divide

---
+++ Multi_Divide_Two_Numbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("Please the first number: "))
+num2 = float(input("Please the second number: "))
+multiResult = num1*num2
+divideResult = num1/num2
+print("Multiply:",num1,"*",num2,"=",multiResult)
+print("Divide:",num1,"/",num2,"=",divideResult)
---
+++ Sum_Sub_Two_Numbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("Please the first number: "))
+num2 = float(input("Please the second number: "))
+sumResult = num1+num2
+subResult = num1-num2
+print("Addition:",num1,"+",num2,"=",sumResult)
+print("Subtraction:",num1,"-",num2,"=",subResult)
